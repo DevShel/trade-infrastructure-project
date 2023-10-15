a76d945a66af129430c630335ed47dc47638fe0f
Sheldon <55363444+DevShel@users.noreply.github.com>
2023-10-15 18:01:11 -0400

    almost finished both scripts

--- docker-side-scripts/packet_reading.py
+++ docker-side-scripts/packet_reading.py
@@ -1,9 +1,33 @@
 from scapy.all import *
 import time
+import pymongo
+from pymongo import MongoClient
+from pymongo import collection
 
-t = AsyncSniffer(prn=lambda x: x.show())
+
+cluster = MongoClient(
+    'mongodb+srv://sheldon:vkc6eqg*CTN!pfa.eyp@trade-infrastructure-cl.xf3vgdx.mongodb.net/trades?retryWrites=true&w=majority')
+db = cluster["trades"]
+collection = db["all-trades"]
+
+
+
+def post_data(packetString, time_received):
+    parsed_array = packetString[2:-2].split('-');
+    print(parsed_array)
+    post = {
+    "amount": parsed_array[2],
+    "latency": time_received - float(parsed_array[3]),
+    "fromCurrency": parsed_array[0],
+    "toCurrency": parsed_array[1]
+    }
+    collection.insert_one(post)
+
+
+t = AsyncSniffer(filter="icmp", prn = lambda x: post_data(str(x.payload[2]), time.time()))
+
 t.start()
 
-time.sleep(15)
-
-t.stop()
+while True:
+    time.sleep(1)
+    exit(t.stop())
--- docker-side-scripts/trade_generation.py
+++ docker-side-scripts/trade_generation.py
@@ -4,13 +4,17 @@ import random, time
 currencyArray = ['USD','EUR','JPY','GBP','AUD','CAD','CHF','CNY','SEK','MXN','NZD','SGD'
 ,'HKD','NOK','KRW','TRY','INR','RUB','BRL','ZAR','DKK','PLN','TWD','THB','MYR'];
 
-# Join trade with syntax: fromCurrency-toCurrency-UNIX_TimeStamp-Amount_of_fromCurrency
+# Join trade with syntax: fromCurrency-toCurrency-Amount_of_fromCurrency-UNIX_TimeStamp
 def generateTrade():
+    currency1 = random.randint(0,24)
+    currency2 = random.choice([i for i in range(0,24) if i not in [currency1]])
+
     return "".join(
+        
         [
-        currencyArray[random.randint(0,24)], 
+        currencyArray[currency1], 
         '-', 
-        currencyArray[random.randint(0,24)],
+        currencyArray[currency2],
         '-',
         str(random.randint(1000,9999)),
         '-',
@@ -19,4 +23,4 @@ def generateTrade():
 
 while True:
     send(IP(src="172.18.0.2", dst="172.18.0.1")/ICMP()/generateTrade())
-    time.sleep(5)
+    time.sleep(2)
