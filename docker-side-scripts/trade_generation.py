fa1b6b4e859631a23f036e5a180830c4a5bc7d0f
Sheldon <55363444+DevShel@users.noreply.github.com>
2023-10-15 15:28:29 -0400

    created trade generation script

--- docker-side-scripts/trade_generation.py
+++ docker-side-scripts/trade_generation.py
@@ -1,4 +1,21 @@
 from scapy.all import *
+import random, time
+
+currencyArray = ['USD','EUR','JPY','GBP','AUD','CAD','CHF','CNY','SEK','MXN','NZD','SGD'
+,'HKD','NOK','KRW','TRY','INR','RUB','BRL','ZAR','DKK','PLN','TWD','THB','MYR'];
+
+# Join trade with syntax: fromCurrency-toCurrency-UNIX_TimeStamp-Amount_of_fromCurrency
+def generateTrade():
+    return "".join(
+        [
+        currencyArray[random.randint(0,24)], 
+        '-', 
+        currencyArray[random.randint(0,24)],
+        '-',
+        str(random.randint(1000,9999)),
+        '-',
+        str(time.time())
+        ])
 
 
-send(IP(src="172.18.0.2", dst="172.18.0.1")/ICMP()/"HI")
+send(IP(src="172.18.0.2", dst="172.18.0.1")/ICMP()/generateTrade())
