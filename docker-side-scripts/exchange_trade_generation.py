bca5b97644097975f12adc1c9e7cb28c6b0b0377
Sheldon <55363444+DevShel@users.noreply.github.com>
2023-10-30 22:15:03 -0500

    cleaning up monitrc file

--- docker-side-scripts/exchange_trade_generation.py
+++ docker-side-scripts/exchange_trade_generation.py
@@ -1,5 +1,16 @@
 from scapy.all import *
 import random, time
+import pymongo
+from pymongo import MongoClient
+from pymongo import collection
+from bson import ObjectId
+
+cluster = MongoClient(
+    'mongodb+srv://sheldon:vkc6eqg*CTN!pfa.eyp@trade-infrastructure-cl.xf3vgdx.mongodb.net/trades?retryWrites=true&w=majority')
+db = cluster["trades"]
+collection = db["status"]
+
+
 
 currencyArray = ['USD','EUR','JPY','GBP','AUD','CAD','CHF','CNY','SEK','MXN','NZD','SGD'
 ,'HKD','NOK','KRW','TRY','INR','RUB','BRL','ZAR','DKK','PLN','TWD','THB','MYR'];
@@ -25,6 +36,22 @@ def generateTrade():
     return output
 
 while True:
-    send((IP(src="172.18.0.3", dst="172.18.0.2")/UDP(dport=5005)/generateTrade()),iface="eth0")
+    currStatus = collection.find_one().get('status')
+    print("Current system status: ", currStatus)
+    if (currStatus == "recording"):
+        send((IP(dst="18.118.122.109")/UDP(dport=RandShort())/generateTrade()))
+    elif (currStatus == "normal"):
+        send((IP(src="172.18.0.3", dst="172.18.0.2")/UDP(dport=5005)/generateTrade()),iface="eth0")
+
     time.sleep(2)
 
+
+
+
+
+
+
+
+
+
+  
